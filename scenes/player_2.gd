e3a4107384742422550f16a7d8932e18a46fe97e
finn8892 <159733074+finn8892@users.noreply.github.com>
2024-08-22 14:59:46 +1200

    g

--- scenes/player_2.gd
+++ scenes/player_2.gd
@@ -2,11 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 350.0
-const JUMP_VELOCITY = -470.0
+const JUMP_VELOCITY = -450.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta):
@@ -15,17 +14,21 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	
-	if direction > 0:
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if  direction > 0:
 		animated_sprite.flip_h= false
-	elif direction < 0:
-		animated_sprite.flip_h= true
+	elif  direction < 0:
+		 animated_sprite.flip_h= true
 		
 	if is_on_floor():
 		if direction == 0:
@@ -45,5 +48,6 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
 
+
+	move_and_slide()
